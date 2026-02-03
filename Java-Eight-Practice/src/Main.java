73264fbcc2681277adadec9c2feb06ded60b8b99
Sravani <sravanireddye4@gmai.com>
2026-02-03 22:43:06 +0530

    new code added

---
+++ Java-Eight-Practice/src/Main.java
@@ -0,0 +1,10 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        javaV8.ExampleStreams st = new javaV8.ExampleStreams();
+        //st.selectOperation();
+        javaV8.ExampleCollectors.collectMethods();
+        //SampleCoding.practice();
+    }
+}
